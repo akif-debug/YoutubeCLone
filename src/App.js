bf948dabd3dc619122736d97a06a22625c8f0a9a
Aakif <akifmansoori8287@gmail.com>
2023-06-18 20:27:49 +0530

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className='text-9xl'>
+            <p className='text-8xl'>dshfkjsdhfkjsdhfkjhsdfkjhsdl;</p>
+        </div>
+    )
+}
+
+export default App
